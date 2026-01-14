d660e031b12021289cea30f9ca1c9ac43d47eddd
cdelahoze <cdelahoze@hotmail.com>
2026-01-13 19:03:39 -0500

    Re-Submit Assiggnment

---
+++ src/TodoList.jsx
@@ -0,0 +1,17 @@
+import { TodoItem } from "./TodoItem";
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && <div className="no-items">No hay tareas</div>}
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          {...todo}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+}
